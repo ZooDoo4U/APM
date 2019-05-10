3a2b384235048c372e91d848fa1fa4c365777add
Dan Doris <dano2k3@hotmail.com>
2019-05-09 20:23:40 -0700

    Initial Commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pm-root',
+  template: `<pm-products></pm-products>` ,
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+  title = 'Angular: Getting Started';
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'
+
+import { AppComponent } from './app.component';
+
+import { ProductsListComponent } from "./products/products-list.component" ;
+import { ConvertToSpacesPipe } from './shared/Convert-to-spaces.pipe';
+import { StarComponent } from './shared/star.component';
+import {  HttpClientModule } from '@angular/common/http';
+
+@NgModule( {
+  declarations:
+  [
+    AppComponent,
+    ProductsListComponent,
+    ConvertToSpacesPipe,
+    StarComponent ],
+  imports: [BrowserModule, FormsModule,HttpClientModule],
+  bootstrap: [AppComponent]
+} )
+
+export class AppModule { }
